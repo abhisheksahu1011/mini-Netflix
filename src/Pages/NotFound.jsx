b8abc5809e7ba3ce0fc7bb8f15ca84b0597efe10
Abhishek sahu <100155445+abhisheksahu1011@users.noreply.github.com>
2023-02-01 14:34:25 +0530

    Create NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 function NotFound() {
   return (
     <div className='d-flex align-item-center justify-content-center'>
-      <h1>Page NotFound😥😥</h1>
+      <h1>Page NotFound</h1>
       <NavLink to='/'> Let me guide to home page</NavLink>
     </div>
   )
